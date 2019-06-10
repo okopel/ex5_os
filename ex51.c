cd50a723a723fcc22692fb9005fafb5646d21327
okopel <okopel@gmail.com>
2019-06-10 11:34:56 -0700

    Final
    Signed-off-by: okopel <okopel@gmail.com>

--- ex51.c
+++ ex51.c
@@ -10,59 +10,70 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <string.h>
+
 #define STDERR 2
 #define ERR_MSG "Error in system call\n"
+#define EX52_FILE "./draw.out"
 
-char getch();
-
-void printError();
-
-void father(int pid, int writer);
-
-
-int main() {
-    int fd[2];
-    pipe(fd);
-    pid_t status = fork();
-    if (status < 0) {
-        printError();
-    } else if (status == 0) {//child
-        dup2(fd[0], 0);
-        execlp("./draw.out", "./draw.out", NULL);
-        printError();
-    } else { //father
-        father(status, fd[1]);
-
-    }
-
-//close(fd[0]);//todo close from father?
-//close(fd[1]);
-}
-
-void printError() {
-    write(STDERR, ERR_MSG, strlen(ERR_MSG));
-}
-
-void father(int status, int pipeWriter) {
-    char c;
-    char buf[1];
-    do {
-        c = getch();
-        if (c == 'a' || c == 's' || c == 'd' || c == 'w' || c == 'q') {
-            buf[0] = c;
-            if (write(pipeWriter, &buf, sizeof(buf)) < 0) {
-                printError();
-            }
-            kill(status, SIGUSR2);
-        }
-    } while (c != 'q');
-}
 
 /**
  * Get char from the user directly without Enter needed.
  * @return a char.
  * NOTE: this function works just in the Terminal
  */
+char getch();
+
+/**
+ * print err msg to the stdin 2
+ */
+void printError();
+
+/**
+ * Get char from keyboard and send them to stdin if legal.
+ * @param pid id of chile in order to send signal
+ * @param writer in order to write the pipe.
+ */
+void father(int pid, int writer);
+
+
+int main() {
+    //pipe address
+    int fd[2];
+    pipe(fd);
+    pid_t status = fork();
+    if (status < 0) {//Error case
+        printError();
+    } else if (status == 0) {//child case
+        dup2(fd[0], 0);//set the writer to EX52
+        execlp(EX52_FILE, EX52_FILE, NULL);
+        printError();
+    } else { //father case
+        father(status, fd[1]);
+    }
+    close(fd[0]);
+    close(fd[1]);
+}
+
+void printError() {
+    write(STDERR, ERR_MSG, strlen(ERR_MSG));
+}
+
+void father(int pid, int writer) {
+    char c;
+    char buf[1];
+    do {
+        c = getch();
+        if (c == 'a' || c == 's' || c == 'd' || c == 'w' || c == 'q') {
+            buf[0] = c;
+            if (write(writer, &buf, sizeof(buf)) < 0) {
+                printError();
+            }
+            kill(pid, SIGUSR2);
+        }
+    } while (c != 'q');
+}
+
+
 char getch() {
     char buf = 0;
     struct termios old = {0};
--- ex52.c
+++ ex52.c
@@ -1,4 +1,3 @@
-
 /************
  * Ori Kopel
  * 205533151
@@ -17,28 +16,71 @@ int place = B_SIZE / 2;
 int placeH = 0;
 bool up = false;
 
-void down();
 
-void initionBoard();
+/**
+ * initialization of the Tetris board.
+ */
+void initializeBoard();
 
-void autoPlay();
-
+/**
+ * erase the last shape before write the new one.
+ */
 void erasePrevStep();
 
-void printBoard();
+/**
+ * draw the shape in the new place.
+ */
+void paintShape();
 
+/**
+ * move the shape a line down.
+ */
+void autoPlay();
 
-void left();
-
-void right();
-
-void rotate();
-
+/**
+ * move the shape by user key.
+ */
 void userPlay();
 
+/**
+ * print the board and the shape to the screen.
+ */
+void printBoard();
 
-//##########################################
+/**
+ * move left the shape.
+ */
+void left();
+
+/**
+ * move right the shape.
+ */
+void right();
+
+/**
+ * move down the shape.
+ */
+void down();
+
+/**
+ * rotate the shape.
+ */
+void rotate();
+
+
+int main() {
+    initializeBoard();
+    signal(SIGUSR2, userPlay);
+    signal(SIGALRM, autoPlay);
+    alarm(1);
+    while (true) {
+        pause();
+    }
+    return 1;
+}
+
 void userPlay() {
+    //the char get from stdin of pipe, from ex51.c
     char c = getchar();
     switch (c) {
         case 'a':
@@ -56,14 +98,13 @@ void userPlay() {
         case 'q':
             exit(1);
         default:
-            //the last step
+            //print again the last step because another key.
             printBoard();
             break;
     }
-
 }
 
-void initionBoard() {
+void initializeBoard() {
     for (int i = 0; i < B_SIZE; i++) {
         board[i][0] = '*';
         board[i][B_SIZE - 1] = '*';
@@ -78,9 +119,11 @@ void initionBoard() {
 }
 
 void autoPlay() {
+    //reset the signal dest
     signal(SIGALRM, autoPlay);
+    //timer to the next step
     alarm(1);
-    //erasePrevStep();
+    //the step every timer
     down();
 }
 
@@ -95,8 +138,7 @@ void erasePrevStep() {
     }
 }
 
-void printBoard() {
-    system("clear");
+void paintShape() {
     board[placeH][place] = '-';
     if (up) {
         board[placeH + 1][place] = '-';
@@ -105,6 +147,11 @@ void printBoard() {
         board[placeH][place + 1] = '-';
         board[placeH][place - 1] = '-';
     }
+}
+
+void printBoard() {
+    system("clear");
+    paintShape();
     for (int i = 0; i < B_SIZE; i++) {
         for (int j = 0; j < B_SIZE; j++) {
             printf("%c", board[i][j]);
@@ -154,12 +201,3 @@ void rotate() {
 }
 
 
-void main() {
-    initionBoard();
-    signal(SIGUSR2, userPlay);
-    signal(SIGALRM, autoPlay);
-    alarm(1);
-    while (true) {
-        pause();
-    }
-}
